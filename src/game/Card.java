fff17aba9acbd3b302045e8d5f7c985309926523
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-15 23:01:39 -0500

    Adding constructor for Card

--- src/game/Card.java
+++ src/game/Card.java
@@ -4,6 +4,11 @@ public class Card {
 	private Suit suit;
 	private Rank rank;
 	
+	public Card(Suit suit, Rank rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+	
 	public Suit getSuit() {
 		return suit;
 	}
