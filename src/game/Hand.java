7ae40b7780d3afda025bee3ee4cf521181ec6f30
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-15 22:55:49 -0500

    Adding the Hand class

---
+++ src/game/Hand.java
@@ -0,0 +1,21 @@
+package game;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Hand {
+
+	private Set<Card> cards;
+	
+	public Hand() {
+		cards = new TreeSet<Card>();
+	}
+	
+	public void insertCard(Card card) {
+		cards.add(card);
+	}
+	
+	public void removeCard(Card card) {
+		cards.remove(card);
+	}
+}
