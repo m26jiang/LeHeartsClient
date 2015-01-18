e1d0b711c55aa6b239beeb845bf9877a4ce12dd6
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-17 22:29:07 -0500

    Adding Window class

---
+++ src/game/Window.java
@@ -0,0 +1,25 @@
+package game;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Window extends JFrame {
+
+	private Window() {
+		this.setTitle("LeHearts");
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);		
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Window();
+			}
+		});
+	}
+}
