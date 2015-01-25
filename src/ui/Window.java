da4213661280b04db70dcebad80d77097c5855b0
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-25 17:14:15 -0500

    functionality

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -4,12 +4,13 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 
-	private Window() {
+	public Window() {
 		this.setTitle("LeHearts");
 		this.setSize(800, 600);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(new GameCanvas());
 		this.setVisible(true);
 	}
 }
