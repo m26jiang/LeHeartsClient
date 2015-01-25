5e08d02086afefb84ddcb79a00da5cf6f51d294c
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-25 17:15:39 -0500

    Create Window in Main class

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,6 +2,8 @@ package game;
 
 import transport.CommandParser;
 import transport.LeHeartsHTTPClient;
+import ui.Window;
+import javax.swing.SwingUtilities;
 
 public class Main {
 
@@ -10,6 +12,14 @@ public class Main {
 		GameController game = new GameController(table);
 		CommandParser parser = new CommandParser(game);
 		LeHeartsHTTPClient client = new LeHeartsHTTPClient(parser, "54.149.107.164", 9001);
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Window();
+			}
+		});
+		
 		client.play();
 	}
 
