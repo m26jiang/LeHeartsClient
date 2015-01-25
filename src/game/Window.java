8d1a63f55e4b37806b357e43ad8a413c220be7c5
Stefan Dumitrescu <stefandumitrescu@hotmail.com>
2015-01-25 12:22:06 -0500

    Remove main method from Window.java

--- src/game/Window.java
+++ src/game/Window.java
@@ -1,7 +1,6 @@
 package game;
 
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 public class Window extends JFrame {
 
@@ -14,12 +13,4 @@ public class Window extends JFrame {
 		this.setVisible(true);		
 	}
 	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Window();
-			}
-		});
-	}
 }
